55c38db4a5d105d4f7be3e3cfc302d485e23f6be
D. Doffy <cuongnsm@gmail.com>
2024-09-19 23:08:05 -0600

    Correct validation when q is empty

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from pydantic import BaseModel
 from fastapi.requests import Request
 
 # connection string to database
-connectionString = "postgresql://postgres:postgres@jetson.local:5432/saokedb"
-engine = create_engine(connectionString, echo=True)
+CONNECTION_STRINGS = "postgresql://postgres:postgres@jetson.local:5432/saokedb"
+engine = create_engine(CONNECTION_STRINGS, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
@@ -20,6 +20,19 @@ def validate(q):
     """
     validate a query
     """
+
+    if not q:
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "status_code": 400,
+                "message": "Query is empty. Please provide a query to search for transactions. Such as /?q=abc",
+                "status": "Bad Request",
+                "total": 0,
+                "result": [],
+            },
+        )
+
     if len(q) < 3:
         raise HTTPException(
             status_code=400,
@@ -49,6 +62,7 @@ def validate(q):
 
 # create a FastAPI app
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -110,69 +124,57 @@ async def read_root(q: str = ''):
     search transactions by term,
     return Not Found if no transaction is found
     """
-    if q is '':
-        raise HTTPException(
-            status_code=404,
-            detail={
-                "status_code": 404,
-                "message": "Not Found, should try with a query. e.g. /?q=abc",
-                "status": "Not Found",
-                "total": 0,
-                "result": [],
-            },
-        )
-    else:
-        validate(q)
+    validate(q)
 
-        with SessionLocal() as session:
-            # replace this by a query to the database
-            query = text(
-                """
-            SELECT date, stt, amount, subject, provider
-            FROM transactions t
-            WHERE to_tsvector('english', subject) @@ plainto_tsquery('english', :q);
+    with SessionLocal() as session:
+        # replace this by a query to the database
+        query = text(
             """
+        SELECT date, stt, amount, subject, provider
+        FROM transactions t
+        WHERE to_tsvector('english', subject) @@ plainto_tsquery('english', :q);
+        """
+        )
+        params = {"q": q}
+
+        # execute the query
+        # and fetch all the results
+        result = session.execute(query, params)
+
+        rows = result.fetchall()
+
+        if len(rows) == 0:
+            raise HTTPException(
+                status_code=404,
+                detail={
+                    "status_code": 404,
+                    "message": "Not Found, no transaction found",
+                    "status": "Not Found",
+                    "total": 0,
+                    "result": [],
+                },
             )
-            params = {"q": q}
-
-            # execute the query
-            # and fetch all the results
-            result = session.execute(query, params)
-
-            rows = result.fetchall()
-
-            if len(rows) == 0:
-                raise HTTPException(
-                    status_code=404,
-                    detail={
-                        "status_code": 404,
-                        "message": "Not Found, no transaction found",
-                        "status": "Not Found",
-                        "total": 0,
-                        "result": [],
-                    },
-                )
-            # Convert rows to a list of dictionaries
-            transactions = [
-                Transaction(
-                    date=row[0],
-                    stt=row[1],
-                    amount=row[2],
-                    subject=row[3],
-                    provider=row[4],
-                )
-                for row in rows
-            ]
-
-            response = TransactionResponse(
-                result=transactions,
-                total=len(transactions),
-                status="success",
-                message="Transactions found",
-                status_code=200,
+        # Convert rows to a list of dictionaries
+        transactions = [
+            Transaction(
+                date=row[0],
+                stt=row[1],
+                amount=row[2],
+                subject=row[3],
+                provider=row[4],
             )
+            for row in rows
+        ]
 
-            return response
+        response = TransactionResponse(
+            result=transactions,
+            total=len(transactions),
+            status="success",
+            message="Transactions found",
+            status_code=200,
+        )
+
+        return response
 
 
 @app.get("/total")
